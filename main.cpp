e49012b86d8bc19037c2e4c885dd1907b193828c
Adam Scroggin <adam.scroggin@subaru-sia.com>
2024-11-26 12:17:06 -0500

    reading in input

--- main.cpp
+++ main.cpp
@@ -4,14 +4,19 @@
 #include "asteroid.h"
 #include <vector>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
 void printSizeOfTypes();
+void testFunction();
+void startGame();
+
+
 
 int main()
 {
-	cout << "Hello World." << endl;
+	startGame();
 
 	auto asteroid1 = new Asteroid();
 	auto asteroid2 = new Asteroid();
@@ -37,6 +42,7 @@ int main()
 	cout << "Asteroid 1 location is X: " << asteroid1->getX() << " Y: " << asteroid1->getY() << endl;
 
 	printSizeOfTypes();
+	testFunction();
 
 	return 0;
 }
@@ -45,4 +51,48 @@ void printSizeOfTypes()
 {
 	cout << "Size Of int is " << sizeof(int) << endl;
 	cout << "Size Of long is " << sizeof(long long) << endl;
+}
+
+void testFunction()
+{
+	auto myMap = new map<int, string>();
+
+	myMap->insert(make_pair(1, "Adam R. Scroggin"));
+	myMap->insert(make_pair(2, "Nina M. Scroggin"));
+	myMap->insert(make_pair(3, "Stella E. Scroggin"));
+
+
+	for (auto& person : *myMap)
+	{
+		cout << "Name is: " << person.second << endl;
+	}
+
+
+}
+
+void startGame()
+{
+	string inputLine;
+	bool badInput = true;
+	int numberOfPlayers = 1;
+
+	cout << "Hello, Welcome to Asteroids!!!" << endl;
+
+
+	while (badInput)
+	{
+		cout << "How many players? Enter 1 or 2" << endl;
+		getline(cin, inputLine);
+
+		if (inputLine == "1")
+		{
+			badInput = false;
+			numberOfPlayers = 1;
+		}
+		else if (inputLine == "2")
+		{
+			badInput = false;
+			numberOfPlayers = 2;
+		}
+	}
 }
